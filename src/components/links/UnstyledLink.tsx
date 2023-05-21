50a54b92bcbda2a16082ab98426b241236abfd0f
LaupWing <loc_strict@hotmail.com>
2023-05-21 09:17:41 +0200

    UnstyledLinkProps

--- src/components/links/UnstyledLink.tsx
+++ src/components/links/UnstyledLink.tsx
@@ -1,4 +1,14 @@
-export const UnstyledLink = () => {
+import { LinkProps } from "next/link"
+import { ComponentPropsWithRef, FC, ReactNode } from "react"
+
+export interface UnstyledLinkProps extends ComponentPropsWithRef<"a">, LinkProps {
+   href: string
+   children: ReactNode
+   openNewTab?: boolean
+   className?: string
+}
+
+export const UnstyledLink:FC<UnstyledLinkProps> = () => {
    return (
       <div>UnstyledLink</div>
    )
