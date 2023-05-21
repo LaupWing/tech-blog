0cdcd07a2644fc810908e3140a660e6dffec9140
LaupWing <loc_strict@hotmail.com>
2023-05-21 16:41:25 +0200

    udpate

--- src/components/Seo.tsx
+++ src/components/Seo.tsx
@@ -5,7 +5,7 @@ const defaultMeta = {
    description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Amet necessitatibus doloribus, omnis delectus obcaecati reprehenderit inventore esse at cum tenetur? Sapiente tenetur molestias odio labore quaerat omnis eos officiis corporis.",
    url: "https://tech.laupwing.com",
    // Need to replace this
-   image: "https://theodorusclarence.com/favicon/large-og.jpg",
+   image: "https://www.cgsusa.org/wp-content/uploads/cropped-placeholder.jpg",
    type: "website",
    robots: "follow, index"
 }
