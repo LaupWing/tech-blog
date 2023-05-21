cf67b7e977a24affb69ca95974c0c3cedc9ad40f
LaupWing <loc_strict@hotmail.com>
2023-05-21 08:34:50 +0200

    udpate

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,27 @@
 /** @type {import('tailwindcss').Config} */
+const withOpacityValue = (variable) => {
+   return ({opacityValue}) => {
+      if(opacityValue === undefined) {
+         return `rgba(var(${variable}))`
+      }
+      return `rgba(var(${variable}) / ${opacityValue})`
+   }
+}
+
 module.exports = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+   content: [
+      "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+      "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+      "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+   ],
+   theme: {
+      extend: {
+         backgroundImage: {
+            "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+            "gradient-conic":
+               "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+         },
       },
-    },
-  },
-  plugins: [],
+   },
+   plugins: [],
 }
