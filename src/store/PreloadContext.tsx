b6ff2f8bb3a673cb45755d747e0740146bd75728
LaupWing <loc_strict@hotmail.com>
2023-05-21 18:56:50 +0200

    update

--- src/store/PreloadContext.tsx
+++ src/store/PreloadContext.tsx
@@ -1,11 +1,21 @@
-import { FC, PropsWithChildren, createContext } from "react"
+import { FC, PropsWithChildren, createContext, useEffect, useState } from "react"
 
 const PreloadContext = createContext<boolean>(false)
 
 export const PreloadProvider:FC<PropsWithChildren> = ({
    children
 }) => {
+   const [preloaded, setPreloaded] = useState(false)
+
+   useEffect(() => {
+      setTimeout(() => {
+         setPreloaded(true)
+      }, 200)
+   }, [])
+
    return (
-      <div>PreloadContext</div>
+      <PreloadContext.Provider value={preloaded}>
+         {children}
+      </PreloadContext.Provider>
    )
 }
