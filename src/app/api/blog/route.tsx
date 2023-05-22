ce28e12f7dcbd2fd6a0f7998c67e45e7574665e4
LaupWing <loc_strict@hotmail.com>
2023-05-22 10:28:53 +0200

    udpate

--- src/app/api/blog/route.tsx
+++ src/app/api/blog/route.tsx
@@ -1,14 +1,14 @@
 import { ImageResponse, NextResponse } from "next/server"
 
 export const inter400 = fetch(
-   new URL("Inter-Regular.ttf", import.meta.url)
+   new URL("@/assets/fonts/Inter-Regular.ttf", import.meta.url)
 ).then((res) => res.arrayBuffer())
 
 export const runtime = "edge"
 
 export async function GET(request: Request) {
    const interRegular = await inter400
-   console.log(interRegular)
+   
    return new ImageResponse(
       <div></div>
    )
