7972d47f7ba324443199dd2895032ac7ebe04be1
LaupWing <loc_strict@hotmail.com>
2023-05-22 17:39:06 +0200

    config

--- src/components/Seo.tsx
+++ src/components/Seo.tsx
@@ -1,16 +1,7 @@
+import { defaultMeta } from "@/config"
 import { useRouter } from "next/navigation"
 import { FC } from "react"
 
-const defaultMeta = {
-   title: "Laup Wing Tech",
-   siteName: "tech.laupwing.com",
-   description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Amet necessitatibus doloribus, omnis delectus obcaecati reprehenderit inventore esse at cum tenetur? Sapiente tenetur molestias odio labore quaerat omnis eos officiis corporis.",
-   url: "https://tech.laupwing.com",
-   // Need to replace this
-   image: "https://www.cgsusa.org/wp-content/uploads/cropped-placeholder.jpg",
-   type: "website",
-   robots: "follow, index"
-}
 
 export interface SeoProps extends Partial<typeof defaultMeta> {
    data?: string
