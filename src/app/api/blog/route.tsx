a1e8a2074516adfe17e2b3012f37fd77dc5d6590
LaupWing <loc_strict@hotmail.com>
2023-05-22 10:34:09 +0200

    udpate

--- src/app/api/blog/route.tsx
+++ src/app/api/blog/route.tsx
@@ -4,10 +4,25 @@ export const inter400 = fetch(
    new URL("@/assets/fonts/Inter-Regular.ttf", import.meta.url)
 ).then((res) => res.arrayBuffer())
 
+export const inter500 = fetch(
+   new URL("@/assets/fonts/Inter-Medium.ttf", import.meta.url)
+).then((res) => res.arrayBuffer())
+
 export const runtime = "edge"
 
 export async function GET(request: Request) {
    const interRegular = await inter400
+   const interMedium = await inter500
+
+   const { searchParams } = new URL(request.url)
+   
+   const templateTitle = searchParams.get("templateTitle")
+   const banner = searchParams.get("banner")
+
+   const query = {
+      templateTitle: templateTitle ?? "Blog Title",
+      banner
+   }
    
    return new ImageResponse(
       <div></div>
