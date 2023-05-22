12d8846f62e0f5619df91aad87fb64ec6aba204d
LaupWing <loc_strict@hotmail.com>
2023-05-22 09:58:21 +0200

    pff

--- src/app/api/blog/route.tsx
+++ src/app/api/blog/route.tsx
@@ -1,15 +1,16 @@
 import { Inter } from "next/font/google"
 import { ImageResponse, NextResponse } from "next/server"
 
-// export const inter400 = fetch(
-//    new URL("../../../assets/fonts/Inter-Regular.ttf", import.meta.url)
-// ).then((res) => res.arrayBuffer())
+export const inter400 = fetch(
+   new URL("Inter-Regular.ttf", import.meta.url)
+).then((res) => res.arrayBuffer())
 
-
+export const runtime = "edge"
 
 export async function GET(request: Request) {
-   // const interRegular = await inter400
-   // console.log(interRegular)
+   const interRegular = await inter400
+   console.log(new URL("Inter-Regular.ttf", import.meta.url))
+   console.log(interRegular)
    return NextResponse.json({
       test: "test"
    })
