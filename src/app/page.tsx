6dc3bee5f33034a33f48ebb76d9e83c3661c936c
LaupWing <loc_strict@hotmail.com>
2023-05-23 11:37:31 +0200

    it works

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,16 @@
+import { getAllFilesFrontmatter } from "@/lib/mdx"
 import { generateRss } from "@/lib/rss"
 
 const fetchFrontmatters = async () => {
    generateRss()
+   const blogs = await getAllFilesFrontmatter("blog")
+   return {
+      blogs
+   }
 }
 
-export default function Home() {
-   const test = fetchFrontmatters()
+export default async function Home() {
+   const test = await fetchFrontmatters()
    console.log(test)
    return (
       <main className="flex min-h-screen flex-col items-center justify-between p-24">
