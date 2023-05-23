15b87e8f5d088ed21279934fe4b823265d6a7cc9
LaupWing <loc_strict@hotmail.com>
2023-05-23 17:26:34 +0200

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -26,31 +26,33 @@ export default async function Home() {
       recentLibrary, 
       recentProjects} = await fetchFrontmatters()
    return (
-      <main className="flex min-h-screen flex-col items-center justify-between p-24">
-         <article className="layout">
-            <h2 className="text-2xl md:text-4xl 2xl:text-5xl">
-               Hallo!
-            </h2>
-            <h1 className="mt-1 text-3xl md:text-5xl 2xl:text-6xl">
-               My name is <Accent>Laup Wing </Accent>
-            </h1>
-            <p className="mt-4 max-w-4xl text-gray-700 dark:text-gray-200 md:mt-6 md:text-lg 2xl:text-xl">
-               I possess a strong enthusiasm for both programming and fitness, finding fulfillment in assisting individuals either at the gym or in the realm of coding.
-            </p>
-            <div className="mt-8 flex flex-wrap gap-4 md:!text-lg">
-               <div className="group relative">
-                  <div
-                     className="absolute -inset-0.5 animate-pulse rounded blur from-[#f1e69a] bg-gradient-to-r to-[#a79a5a] opacity-75 transition duration-1000 group-hover:opacity-100 group-hover:duration-200"
-                  />
-                  <ButtonLink href="#intro">
-                     Read the Blog
+      <main>
+         <section className="flex min-h-screen flex-col items-center justify-center -mt-20 mb-20">
+            <article className="layout">
+               <h2 className="text-2xl md:text-4xl 2xl:text-5xl">
+                  Hallo!
+               </h2>
+               <h1 className="mt-1 text-3xl md:text-5xl 2xl:text-6xl">
+                  My name is <Accent>Laup Wing </Accent>
+               </h1>
+               <p className="mt-4 max-w-4xl text-gray-700 dark:text-gray-200 md:mt-6 md:text-lg 2xl:text-xl">
+                  I possess a strong enthusiasm for both programming and fitness, finding fulfillment in assisting individuals either at the gym or in the realm of coding.
+               </p>
+               <div className="mt-8 flex flex-wrap gap-4 md:!text-lg">
+                  <div className="group relative">
+                     <div
+                        className="absolute -inset-0.5 animate-pulse rounded blur from-[#f1e69a] bg-gradient-to-r to-[#a79a5a] opacity-75 transition duration-1000 group-hover:opacity-100 group-hover:duration-200"
+                     />
+                     <ButtonLink href="#intro">
+                        Read the Blog
+                     </ButtonLink>
+                  </div>
+                  <ButtonLink href="/about">
+                     Learn more about me
                   </ButtonLink>
                </div>
-               <ButtonLink href="/about">
-                  Learn more about me
-               </ButtonLink>
-            </div>
-         </article>
+            </article>
+         </section>
       </main>
    )
 }
