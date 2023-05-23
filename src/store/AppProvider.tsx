1e08d7c6cfe040bd88563ecca346f69775984ca0
LaupWing <loc_strict@hotmail.com>
2023-05-23 10:33:54 +0200

    allllrite

--- src/store/AppProvider.tsx
+++ src/store/AppProvider.tsx
@@ -2,13 +2,21 @@
 import { ThemeProvider } from "next-themes"
 import { PreloadProvider } from "./PreloadContext"
 import { FC, PropsWithChildren } from "react"
+import { SWRConfig } from "swr"
+import axios from "axios"
 
-export const AppProvider:FC<PropsWithChildren> = ({ children }) => {
+export const AppProvider: FC<PropsWithChildren> = ({ children }) => {
    return (
       <ThemeProvider attribute="class" defaultTheme="light">
          <PreloadProvider>
-            {children}
+            <SWRConfig
+               value={{
+                  fetcher: (url) => axios.get(url).then((res) => res.data),
+               }}
+            >
+               {children}
+            </SWRConfig>
          </PreloadProvider>
-      </ThemeProvider> 
+      </ThemeProvider>
    )
 }
