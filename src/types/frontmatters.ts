b5e3ffb8973686f346b9b463ad6ac8358975fc6c
LaupWing <loc_strict@hotmail.com>
2023-05-23 10:48:12 +0200

    ProjectFrontmatter

--- src/types/frontmatters.ts
+++ src/types/frontmatters.ts
@@ -1,4 +1,30 @@
+import { ReadTimeResults } from "reading-time"
 
+export interface BlogFrontmatter {
+   wordCount: number
+   readingTime: ReadTimeResults
+   slug: string
+   title: string
+   description: string
+   banner: string
+   publishedAt: string
+   lastUpdated?: string
+   tags: string
+   repost?: string
+}
+
+export interface ProjectFrontmatter {
+   slug: string
+   title: string
+   publishedAt: string
+   lastUpdated?: string
+   description: string
+   category?: string
+   techs: string
+   banner: string
+   link?: string
+   github?: string
+}
 
 
 export type ContentType = "blog" | "library" | "projects"
