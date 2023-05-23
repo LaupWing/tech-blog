79e146e6171f0024a660669c97e44cbe681eed36
LaupWing <loc_strict@hotmail.com>
2023-05-23 17:35:32 +0200

    icons

--- src/components/Icons.tsx
+++ src/components/Icons.tsx
@@ -1,2 +1,18 @@
-import { IoNewspaperSharp } from "react-icons/io5"
+import { IoArrowDownOutline, IoNewspaperSharp } from "react-icons/io5"
+import { IconType } from "react-icons"
+
+export const IconNewspaper:IconType = (props) => {
+   return (
+      <IoNewspaperSharp 
+         {...props}
+      />
+   )
+}
+export const IconArrowDown:IconType = (props) => {
+   return (
+      <IoArrowDownOutline
+         {...props}
+      />
+   )
+}
 
