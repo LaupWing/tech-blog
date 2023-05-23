72538d8303d1fa798f59c9a26f642a8e28a417b2
LaupWing <loc_strict@hotmail.com>
2023-05-23 19:03:04 +0200

    update

--- src/components/buttons/ThemeButton.tsx
+++ src/components/buttons/ThemeButton.tsx
@@ -1,11 +1,18 @@
 "use client"
+import clsx from "clsx"
 import { useTheme } from "next-themes"
+import { IconMoon } from "../Icons"
 
 export const ThemeButton = () => {
    const { theme } = useTheme()
-   console.log(theme)
 
    return (
-      <button>ThemeButton</button>
+      <button
+         className={clsx(
+            "rounded-md p-2 focus:outline-none md:p-2.5 border dark:border-gray-600 hover:border-accent hover:text-accent text-lg md:text-xl"
+         )}
+      >
+         <IconMoon />
+      </button>
    )
 }
