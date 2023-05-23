c0fb749b0378facdcbf366e9a5a2a96e4800ba7e
LaupWing <loc_strict@hotmail.com>
2023-05-23 11:15:08 +0200

    installed gray matter

--- src/lib/mdx.ts
+++ src/lib/mdx.ts
@@ -1,8 +1,16 @@
-import { ContentType } from "@/types/frontmatters"
-import { readdirSync } from "fs"
+import { ContentType, PickFrontmatter } from "@/types/frontmatters"
+import { readFileSync, readdirSync } from "fs"
 import { join } from "path"
 
 
 export async function getAllFilesFrontmatter<T extends ContentType>(type: T) {
    const files = readdirSync(join(process.cwd(), "src", "contents", type))
+
+   return files.reduce((allPosts: Array<PickFrontmatter<T>>, postSlug) => {
+      const source = readFileSync(
+         join(process.cwd(), "src", "contents", type, postSlug),
+         "utf-8"
+      )
+      // const {}
+   })
 }
