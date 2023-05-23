4d065e8e7db8836b83e89b1093eb6548a93698d2
LaupWing <loc_strict@hotmail.com>
2023-05-23 11:34:10 +0200

    hm

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,8 @@ const fetchFrontmatters = async () => {
 }
 
 export default function Home() {
+   const test = fetchFrontmatters()
+   console.log(test)
    return (
       <main className="flex min-h-screen flex-col items-center justify-between p-24">
          
