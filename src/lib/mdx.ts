7e78d1d67d475211a77ee556b64a234d2d0a7477
LaupWing <loc_strict@hotmail.com>
2023-05-23 11:08:50 +0200

    update

--- src/lib/mdx.ts
+++ src/lib/mdx.ts
@@ -1,5 +1,8 @@
+import { ContentType } from "@/types/frontmatters"
+import { readdirSync } from "fs"
+import { join } from "path"
 
 
-// export async function getAllFilesFrontmatter(params:type) {
-   
-// }
+export async function getAllFilesFrontmatter<T extends ContentType>(type: T) {
+   const files = readdirSync(join(process.cwd(), "src", "contents", type))
+}
