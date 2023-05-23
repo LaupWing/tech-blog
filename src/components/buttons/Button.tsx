5769a4bfa877d0b7b713d47556ddeba0a8af57f7
LaupWing <loc_strict@hotmail.com>
2023-05-23 17:03:43 +0200

    Button

--- src/components/buttons/Button.tsx
+++ src/components/buttons/Button.tsx
@@ -1,13 +1,11 @@
+import clsx from "clsx"
 import { ComponentPropsWithoutRef, FC } from "react"
 
-enum ButtonVariant {
-   "default",
-   "filled"
-}
+type ButtonVariant = "default" | "gold"
 
 interface ButtonProps extends ComponentPropsWithoutRef<"button"> {
    isLoading: boolean
-   variant?: keyof typeof ButtonVariant
+   variant?: ButtonVariant
 }
 
 export const Button:FC<ButtonProps> = ({
@@ -20,12 +18,21 @@ export const Button:FC<ButtonProps> = ({
 }) => {
    const disabled = isLoading || buttonDisabled
 
+   const variants: Record<ButtonVariant, string> = {
+      default: "bg-primary text-gray-600 disabled:bg-gray-200 dark:text-gray-200 dark:disabled:bg-gray-700",
+      gold: ""
+   }
+
    return (
       <button
          {...props}
          disabled={disabled}
+         className={clsx(
+            "rounded px-4 py-2 font-bold border border-gray-300 shadow-sm dark:border-gray-600 focus:outline-none focus-visible:ring focus-visible:ring-contrast scale-100 hover:scale-[1.03] active:scale-[0.97] motion-safe:transform-gpu motion-reduce:hover:scale-100 transition duration-100",
+            variants[variant]
+         )}
       >
-         Button
+         {children}
       </button>
    )
 }
