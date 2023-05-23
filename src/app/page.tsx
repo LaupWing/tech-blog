be4291edee3bcda76e6f1c9da4cf224d29083888
LaupWing <loc_strict@hotmail.com>
2023-05-23 17:20:34 +0200

    updatre

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import { Accent } from "@/components/Accent"
+import { ButtonLink } from "@/components/links"
 import { getAllFilesFrontmatter, getRecent } from "@/lib/mdx"
 import { generateRss } from "@/lib/rss"
 
@@ -36,6 +37,19 @@ export default async function Home() {
             <p className="mt-4 max-w-4xl text-gray-700 dark:text-gray-200 md:mt-6 md:text-lg 2xl:text-xl">
                I possess a strong enthusiasm for both programming and fitness, finding fulfillment in assisting individuals either at the gym or in the realm of coding.
             </p>
+            <div className="mt-8 flex flex-wrap gap-4 md:!text-lg">
+               <div className="group relative">
+                  <div
+                     className="absolute -inset-0.5 animate-pulse rounded blur from-[#f1e69a] bg-gradient-to-r to-[#a79a5a] opacity-75 transition duration-1000 group-hover:opacity-100 group-hover:duration-200"
+                  />
+                  <ButtonLink href="#intro">
+                     Read the Blog
+                  </ButtonLink>
+               </div>
+               <ButtonLink href="/about">
+                  Learn more about me
+               </ButtonLink>
+            </div>
          </article>
       </main>
    )
