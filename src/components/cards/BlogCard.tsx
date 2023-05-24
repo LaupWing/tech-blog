8d91bdd4f00b8fa3168df4be8151cd9e0c6c4df3
LaupWing <loc_strict@hotmail.com>
2023-05-24 11:32:09 +0200

    rag

--- src/components/cards/BlogCard.tsx
+++ src/components/cards/BlogCard.tsx
@@ -3,6 +3,7 @@ import clsx from "clsx"
 import { ComponentPropsWithoutRef, FC } from "react"
 import { UnstyledLink } from "../links"
 import { CloudinaryImage } from "../images"
+import { Tag } from "../elements"
 
 interface BlogCardProps extends ComponentPropsWithoutRef<"li"> {
    post: BlogFrontmatter & InjectedMeta
@@ -16,23 +17,38 @@ export const BlogCard:FC<BlogCardProps> = ({
    onClick
 }) => {
    return (
-      <li className={clsx("w-full rounded-md border border-gray-300 bg-primary dark:border-gray-600 scale-100 hover:scale-[1.02] active:scale-[0.97] motion-safe:transform-gpu transition duration-100 motion-reduce:hover:scale-100 animate-shadow", className)}>
+      <li 
+         className={clsx(
+            "w-full rounded-md border border-gray-300 bg-primary dark:border-gray-600 scale-100 hover:scale-[1.02] active:scale-[0.97] motion-safe:transform-gpu transition duration-100 motion-reduce:hover:scale-100 animate-shadow",
+            className
+         )}
+         onClick={onClick}
+      >
          <UnstyledLink
             className="block h-full rounded-md focus-within:outline-none focus-visible:ring focus-visible:ring-contrast"
             href={`/blog/${post.slug}`}
          >
-            <CloudinaryImage 
-               noStyle
-               className="rounded-t-md overflow-hidden pointer-events-none"
-               publicId="samples/bike"
-               alt="Bike"
-               width={1200}
-               height={(1200 * 2) /5}
-               aspect={{
-                  height: 2,
-                  width: 5
-               }}
-            />
+            <div className="relative">
+               <CloudinaryImage 
+                  noStyle
+                  className="rounded-t-md overflow-hidden pointer-events-none"
+                  publicId="samples/bike"
+                  alt="Bike"
+                  width={1200}
+                  height={(1200 * 2) /5}
+                  aspect={{
+                     height: 2,
+                     width: 5
+                  }}
+               />
+               <div className="absolute bottom-0 px-4 w-full py-2 mt-2 flex flex-wrap justify-end gap-x-2 gap-y-1 text-sm text-black dark:text-gray-100">
+                  {post.tags.split(",").map((tag) => (
+                     <Tag
+                     
+                     />
+                  ))}
+               </div>
+            </div>
          </UnstyledLink>
       </li>
    )
