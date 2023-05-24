70a41320a0943a52cf91fd751c584eacc5c07e52
LaupWing <loc_strict@hotmail.com>
2023-05-24 08:49:20 +0200

    udpate

--- src/components/buttons/ThemeButton.tsx
+++ src/components/buttons/ThemeButton.tsx
@@ -1,18 +1,30 @@
 "use client"
 import clsx from "clsx"
 import { useTheme } from "next-themes"
-import { IconMoon } from "../Icons"
+import { IconMoon, IconSun } from "../Icons"
+import { useEffect, useState } from "react"
 
 export const ThemeButton = () => {
-   const { theme } = useTheme()
+   const { theme, setTheme } = useTheme()
+   const [mounted, setMounted] = useState(false) 
+
+   useEffect(() => {
+      setMounted(true)
+   }, [])
 
    return (
       <button
          className={clsx(
             "rounded-md p-2 focus:outline-none md:p-2.5 border dark:border-gray-600 hover:border-accent-dark hover:text-accent-dark text-lg md:text-xl"
          )}
+         onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
       >
-         <IconMoon />
+         {mounted 
+            ?  <>
+                  {theme === "light" ? <IconMoon /> : <IconSun />}
+               </> 
+            : <IconSun/>
+         }
       </button>
    )
 }
