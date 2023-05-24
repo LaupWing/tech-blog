8de9b788259bf90b8e5744fd0a3e0d383430d15e
LaupWing <loc_strict@hotmail.com>
2023-05-24 08:45:26 +0200

    udpat

--- src/components/buttons/ThemeButton.tsx
+++ src/components/buttons/ThemeButton.tsx
@@ -9,7 +9,7 @@ export const ThemeButton = () => {
    return (
       <button
          className={clsx(
-            "rounded-md p-2 focus:outline-none md:p-2.5 border dark:border-gray-600 hover:border-accent hover:text-accent text-lg md:text-xl"
+            "rounded-md p-2 focus:outline-none md:p-2.5 border dark:border-gray-600 hover:border-accent-dark hover:text-accent-dark text-lg md:text-xl"
          )}
       >
          <IconMoon />
