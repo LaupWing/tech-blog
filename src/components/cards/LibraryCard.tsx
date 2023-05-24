da8b88e2db5b43a0916552fe0df595fb840d2442
LaupWing <loc_strict@hotmail.com>
2023-05-24 15:17:51 +0200

    udpate

--- src/components/cards/LibraryCard.tsx
+++ src/components/cards/LibraryCard.tsx
@@ -1,4 +1,11 @@
-export const LibraryCard = () => {
+import { ComponentPropsWithoutRef, FC } from "react"
+import { InjectedMeta, LibraryFrontmatter } from "@/types/frontmatters"
+
+interface LibraryCardProps extends ComponentPropsWithoutRef<"li"> {
+   snippet: LibraryFrontmatter & InjectedMeta
+}
+
+export const LibraryCard:FC<LibraryCardProps> = () => {
    return (
       <div>LibraryCard</div>
    )
--- src/components/cards/index.ts
+++ src/components/cards/index.ts
@@ -1,3 +1,5 @@
 export { BlogCard } from "./BlogCard"
 export { ProjectCard } from "./ProjectCard"
 export { LibraryCard } from "./LibraryCard"
+
+
