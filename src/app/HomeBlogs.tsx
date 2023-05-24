3be3eb8d7a8b9c240eafdce171e6ee00d0f8a2f5
LaupWing <loc_strict@hotmail.com>
2023-05-24 09:08:30 +0200

    udpate

---
+++ src/app/HomeBlogs.tsx
@@ -0,0 +1,14 @@
+import { Accent } from "@/components/Accent"
+
+const HomeBlogs = () => {
+   return (
+      <section className="py-20">
+         <article className="layout">
+            <h2 id="projects" className="text-2xl md:text-4xl">
+               <Accent>Recent Blog Posts</Accent>
+            </h2>
+         </article>
+      </section>
+   )
+}
+export default HomeBlogs
