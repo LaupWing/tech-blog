f8d5dcf88ae2a42bb09c1ae5ec20afdd383d7b72
LaupWing <loc_strict@hotmail.com>
2023-05-24 12:23:22 +0200

    imports

---
+++ src/components/TechIcons.tsx
@@ -0,0 +1,19 @@
+import {
+   SiFirebase,
+   SiGit,
+   SiGoogleanalytics,
+   SiJavascript,
+   SiMarkdown,
+   SiMongodb,
+   SiNextdotjs,
+   SiNodedotjs,
+   SiNotion,
+   SiPrettier,
+   SiReact,
+   SiRedux,
+   SiSass,
+   SiSwift,
+   SiTailwindcss,
+   SiTypescript,
+ } from "react-icons/si"
+ import { IoLogoVercel } from "react-icons/io5"
