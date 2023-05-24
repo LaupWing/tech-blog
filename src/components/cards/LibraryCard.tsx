07fdfe3ee346db5567487e1b244e0052b45d04e1
LaupWing <loc_strict@hotmail.com>
2023-05-24 15:11:14 +0200

    Exprots library card

---
+++ src/components/cards/LibraryCard.tsx
@@ -0,0 +1,5 @@
+export const LibraryCard = () => {
+   return (
+      <div>LibraryCard</div>
+   )
+}
--- src/components/cards/index.ts
+++ src/components/cards/index.ts
@@ -1,2 +1,3 @@
 export { BlogCard } from "./BlogCard"
 export { ProjectCard } from "./ProjectCard"
+export { LibraryCard } from "./LibraryCard"
