a22d1eea52f816b9ccbf8e387b456e4db5341a99
LaupWing <loc_strict@hotmail.com>
2023-05-24 09:56:42 +0200

    udpate

--- src/components/cards/BlogCard.tsx
+++ src/components/cards/BlogCard.tsx
@@ -1,5 +1,11 @@
+import { BlogFrontmatter } from "@/types/frontmatters"
+
+interface BlogcardProps {
+   post: BlogFrontmatter
+}
+
 export const BlogCard = () => {
-  return (
-    <div>BlogCard</div>
-  )
+   return (
+      <div>BlogCard</div>
+   )
 }
