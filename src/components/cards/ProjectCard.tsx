e1536f2428448fd501aeeadc1d680112f174235a
LaupWing <loc_strict@hotmail.com>
2023-05-24 12:05:04 +0200

    update

---
+++ src/components/cards/ProjectCard.tsx
@@ -0,0 +1,12 @@
+import clsx from "clsx"
+
+export const ProjectCard = () => {
+   return (
+      <li className={clsx(
+         "rounded-md md:w-full border dark:border-gray-600 scale-100 hover:scale-[1.02] active:scale-[0.97] motion-safe:transform-gpu transition duration-100 motion-reduce:hover:scale-100 animate-shadow"
+         )}
+      >
+         
+      </li>
+   )
+}
