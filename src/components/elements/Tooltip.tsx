ac4a47d3c5204c4537ca51d022d03fbdb871b93b
LaupWing <loc_strict@hotmail.com>
2023-05-24 14:02:41 +0200

    border

--- src/components/elements/Tooltip.tsx
+++ src/components/elements/Tooltip.tsx
@@ -1,5 +1,6 @@
 import { TooltipProps, Tooltip as TippyTooltip } from "react-tippy"
 import { ComponentPropsWithoutRef, ReactNode, FC } from "react"
+import clsx from "clsx"
 
 type TooltipTextProps = {
    tipChildren: ReactNode
@@ -22,13 +23,24 @@ export const Tooltip:FC<TooltipTextProps> = ({
          trigger="mouseenter"
          interactive
          html={
-            <div>
+            <div className={clsx(className, "inline-block rounded-md bg-primary p-2 text-gray-600 shadow-md dark:text-gray-200 border dark:border-gray-600")}>
                {tipChildren}
             </div>
          }  
          {...props}
       >
-
+         {withUnderline ? (
+            <span 
+               className={clsx(spanClassName, "underline")}
+               style={{
+                  textDecorationStyle: "dotted"
+               }}
+            >
+               {children}
+            </span>
+         ) : (
+            <>{children}</>
+         )}
       </TippyTooltip>
    )
 }
