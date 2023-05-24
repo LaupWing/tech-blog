b0414238ae96853e3ad82aa8692c25b7002685fe
LaupWing <loc_strict@hotmail.com>
2023-05-24 12:05:43 +0200

    update

--- src/components/cards/ProjectCard.tsx
+++ src/components/cards/ProjectCard.tsx
@@ -1,6 +1,12 @@
+import { ProjectFrontmatter } from "@/types/frontmatters"
 import clsx from "clsx"
+import { ComponentPropsWithoutRef, FC } from "react"
 
-export const ProjectCard = () => {
+interface ProjectCardProps extends ComponentPropsWithoutRef<"li"> {
+   project: ProjectFrontmatter
+}
+
+export const ProjectCard:FC<ProjectCardProps> = () => {
    return (
       <li className={clsx(
          "rounded-md md:w-full border dark:border-gray-600 scale-100 hover:scale-[1.02] active:scale-[0.97] motion-safe:transform-gpu transition duration-100 motion-reduce:hover:scale-100 animate-shadow"
