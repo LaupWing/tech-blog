4ca5b0b7f37f44959768e71a66e31e7c03b5ce19
LaupWing <loc_strict@hotmail.com>
2023-05-25 16:21:28 +0200

    update

--- src/app/blog/BlogsContainer.tsx
+++ src/app/blog/BlogsContainer.tsx
@@ -1,16 +1,25 @@
 "use client"
+import { Tag } from "@/components/elements"
 import { getTags } from "@/lib/mdx-client"
-import { FrontmatterWithTags } from "@/types/frontmatters"
-import { FC } from "react"
+import { BlogFrontmatter, FrontmatterWithTags, InjectedMeta } from "@/types/frontmatters"
+import { ChangeEvent, FC, useState } from "react"
 
 interface BlogsContainerProps {
-   blogs: Array<FrontmatterWithTags>
+   blogs: Array<BlogFrontmatter>
 }
 
 const BlogsContainer:FC<BlogsContainerProps> = ({
    blogs
 }) => {
    const tags = getTags(blogs)
+   const [search, setSearch] = useState<string>("")
+   const [filteredBlogs, setFilteredBlogs] = useState<Array<BlogFrontmatter & InjectedMeta>>(blogs)
+
+   const handleSearch = (e: ChangeEvent<HTMLInputElement>) => {
+      setSearch(e.target.value)
+   }
+
+   const filteredTags = getTags(filteredBlogs)
 
    return (
       <>
@@ -18,7 +27,19 @@ const BlogsContainer:FC<BlogsContainerProps> = ({
             type="text" 
             className="w-full rounded-md dark:bg-dark border border-gray-300 dark:border-gray-600 focus:border-accent-dark focus:outline-none focus:ring-0 dark:focus:border-accent-light mt-4"
             placeholder="Search..."
+            value={search}
+            onChange={handleSearch}
          />
+         <div className="mt-2 flex flex-wrap items-baseline justify-start gap-2 text-sm text-gray-600 dark:text-gray-300">
+            <span className="font-medium">Choose topic:</span>
+            {tags.map((tag) => (
+               <Tag
+                  key={tag}
+               >
+                  {tag}
+               </Tag>
+            ))}
+         </div>
       </>
    )
 }
