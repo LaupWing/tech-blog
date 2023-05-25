bda91b22f90b46b135df6f0b8f62fecc5286a880
LaupWing <loc_strict@hotmail.com>
2023-05-25 19:07:25 +0200

    update

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,10 +1,35 @@
+import { CloudinaryImage } from "@/components/images"
 import { getFiles } from "@/lib/mdx"
 
-const SingleBlogPage = () => {
+const fetchPost = async (slug: string) => {
+   // const params = useParams()
+   // console.log(params)
+} 
+
+interface PageProps {
+   params: {
+      slug: string
+   }
+}
+
+const SingleBlogPage = async (props: PageProps) => {
+   const post = await fetchPost(props.params.slug)
    return (
       <main>
-         <section>
-            
+         <section className="layout">
+            <div className="pb-4">
+               <CloudinaryImage
+                  publicId="samples/bike"
+                  alt="Bike"
+                  width={1200}
+                  height={(1200 * 2) /5}
+                  aspect={{
+                     height: 2,
+                     width: 5
+                  }}
+               />
+               <h1 className="mt-4"></h1>
+            </div>
          </section>
       </main>
    )
