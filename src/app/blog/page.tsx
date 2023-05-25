0bec83c4951d112a5aa7daa3e1a211002883f7e6
LaupWing <loc_strict@hotmail.com>
2023-05-25 10:14:50 +0200

    blog

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,6 +1,14 @@
+import { Accent } from "@/components/elements/Accent"
+
 const Blog = () => {
    return (
-      <div>Blog</div>
+      <main>
+         <section className="layout py-12">
+            <h1 className="text-3xl md:text-5xl">
+               <Accent>Blog</Accent>
+            </h1>
+         </section>
+      </main>
    )
 }
 export default Blog
