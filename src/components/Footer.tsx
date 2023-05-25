d0e9ed13a70b5eeea4718f34851d37f62cfb2091
LaupWing <loc_strict@hotmail.com>
2023-05-25 09:38:12 +0200

    udpate

---
+++ src/components/Footer.tsx
@@ -0,0 +1,13 @@
+import { FC } from "react"
+
+export const Footer:FC = () => {
+   return (
+      <footer className="mt-4 pb-2">
+         <main className="layout flex flex-col items-center border-t pt-6 dark:border-gray-600">
+            <p className="mt-12 font-medium text-gray-600 dark:text-gray-300">
+               Reach Out
+            </p>
+         </main>
+      </footer>
+   )
+}
