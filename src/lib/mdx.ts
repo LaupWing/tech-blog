840844560cc618b1a41fd87f173bc12495479772
LaupWing <loc_strict@hotmail.com>
2023-05-25 19:24:26 +0200

    getFileBySlug

--- src/lib/mdx.ts
+++ src/lib/mdx.ts
@@ -4,13 +4,44 @@ import matter from "gray-matter"
 import { bundleMDX } from "mdx-bundler"
 import { join } from "path"
 import readingTime from "reading-time"
+import rehypeAutolinkHeadings from "rehype-autolink-headings/lib"
+import rehypePrism from "rehype-prism-plus/."
+import rehypeSlug from "rehype-slug"
+import remarkGfm from "remark-gfm"
 
 export async function getFileBySlug(type: ContentType, slug: string) {
    const source = readFileSync(join(process.cwd(), "src", type, `${slug}.mdx`), "utf-8")
  
-   const {} = await bundleMDX({
-      source
+   const { code, frontmatter } = await bundleMDX({
+      source,
+      mdxOptions(options) {
+         options.remarkPlugins = [...(options?.remarkPlugins ?? []), remarkGfm]
+         options.rehypePlugins = [
+            ...(options?.rehypePlugins ?? []),
+            rehypeSlug,
+            rehypePrism,
+            [
+               rehypeAutolinkHeadings,
+               {
+                  properties: {
+                     className: ["hash-anchor"]
+                  }
+               }
+            ]
+         ]
+         return options
+      }
    })
+
+   return {
+      code,
+      frontmatter: {
+         wordCount: source.split(/\s+/gu).length,
+         readingTime: readingTime(source),
+         slug: slug || null,
+         ...frontmatter
+      }
+   }
 }
 
 export async function getAllFilesFrontmatter<T extends ContentType>(type: T) {
