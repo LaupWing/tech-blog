326b680b8a326a908f6206560f2f8238addf7ac6
LaupWing <loc_strict@hotmail.com>
2023-05-26 10:36:47 +0200

    udpate

--- src/components/MDXComponents.tsx
+++ src/components/MDXComponents.tsx
@@ -1,5 +1,15 @@
+import { CustomLink } from "./links/CustomLink"
+import Image from "next/image"
+import { Pre } from "./sections"
+import { CustomCode } from "./sections/CustomCode"
+import { CloudinaryImage } from "./images"
 
 
 export const MDXComponents = {
-   // a: CustomLInk
+   a: CustomLink,
+   Image,
+   pre: Pre,
+   code: CustomCode,
+   CloudinaryImage,
+   
 }
