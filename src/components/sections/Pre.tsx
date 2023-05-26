82ea1d05bdb524b2ed09b23e325e9bf974ed1f93
LaupWing <loc_strict@hotmail.com>
2023-05-26 10:08:58 +0200

    pre

---
+++ src/components/sections/Pre.tsx
@@ -0,0 +1,17 @@
+import { ComponentPropsWithoutRef, FC } from "react"
+
+interface PreProps extends ComponentPropsWithoutRef<"pre"> {}
+
+export const Pre:FC<PreProps> = (props) => {
+   return (
+      <pre {...props}>
+         {props.children}
+         <style jsx>{`
+            pre {
+               position: relative;
+               position-top: 2.5rem;
+            }
+         `}</style>
+      </pre>
+   )
+}
