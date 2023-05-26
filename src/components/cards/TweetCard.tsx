2ab6e4fe7b361a41a496685e7f2181a68be55e25
LaupWing <loc_strict@hotmail.com>
2023-05-26 10:47:58 +0200

    wteetcard

--- src/components/cards/TweetCard.tsx
+++ src/components/cards/TweetCard.tsx
@@ -1,4 +1,18 @@
+import clsx from "clsx"
+import { FC } from "react"
+import { TweetProps, Tweet } from "react-twitter-widgets"
 
-interface TweetCardProps {
+interface TweetCardProps extends TweetProps {
    className?: string
+}
+
+export const TweetCard:FC<TweetCardProps> = ({
+   tweetId,
+   className
+}) => {
+   return (
+      <div className={clsx("not-prose w-[99%]", className)}>
+         <Tweet tweetId={tweetId} />
+      </div>
+   )
 }
