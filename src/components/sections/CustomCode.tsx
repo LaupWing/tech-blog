28fbc7a6939c2bed4cf21dab0c095186941fde31
LaupWing <loc_strict@hotmail.com>
2023-05-26 10:23:38 +0200

    lnaguage

--- src/components/sections/CustomCode.tsx
+++ src/components/sections/CustomCode.tsx
@@ -6,13 +6,33 @@ interface CustomCodeProps extends ComponentPropsWithoutRef<"code">{}
 
 export const CustomCode:FC<CustomCodeProps> = ({
    className,
-   children
+   children,
+   ...props
 }) => {
    const textRef = useRef<HTMLDivElement>(null)
    const [isCopied, setIsCopied] = useState<boolean>(false)
-   const [copy] = useCopyToClipboard() 
+   const [copy] = useCopyToClipboard()
+
+   const language = className?.includes("language")
+      ? className.replace("language-", "").replace(" code-highlight", "")
+      : null
 
    return (
-      <div>CustomCode</div>
+      <code {...props} data-code-type={language && "code-block"}>
+         {language ? (
+            <div ref={textRef} className="overflow-x-auto">
+               {children}
+            </div>
+         ) : (
+            <span>{children}</span>
+         )}
+         {language && (
+            <div className="absolute left-6 top-0 rounded-b-md border border-t-0 border-gray-600 px-3 py-1">
+               <span className="select-none bg-gradient-to-tr from-accent-light to-accent-dark bg-clip-text font-medium text-transparent">
+                  {language}
+               </span>
+            </div>
+         )}
+      </code>
    )
 }
