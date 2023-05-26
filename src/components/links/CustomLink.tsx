ce8b42156948008fc15ab300ee1ad8e15c168256
LaupWing <loc_strict@hotmail.com>
2023-05-26 10:35:02 +0200

    Customlink

---
+++ src/components/links/CustomLink.tsx
@@ -0,0 +1,20 @@
+import { FC } from "react"
+import { UnstyledLink, UnstyledLinkProps } from "./UnstyledLink"
+import clsx from "clsx"
+
+export const CustomLink:FC<UnstyledLinkProps> = ({
+   children,
+   className,
+   ...props
+}) => {
+   return (
+      <UnstyledLink
+         {...props}
+         className={clsx("animated-underline inline-flex items-center font-medium focus:outline-none focus-visible:ring focus-visible:ring-accent-dark dark:focus-visible:ring-accent-light border-b border-dotted border-dark hover:border-dark/0", className)}
+      >
+         <span className="dark:bg-gradient-to-tr dark:from-accent-light dark:to-accent-dark dark:bg-clip-text dark:text-transparent">
+            {children}
+         </span>
+      </UnstyledLink>
+   )
+}
