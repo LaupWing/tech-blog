cba32ee51a2451ee8207cc4ea3b3c5bb8670a4e0
LaupWing <loc_strict@hotmail.com>
2023-05-27 09:02:25 +0200

    dupate

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,6 +1,15 @@
+import { Accent } from "@/components/elements/Accent"
+
 const Projects = () => {
    return (
-      <div>Projects</div>
+      <section className="layout py-12">
+         <h1 className="text-3xl md:text-5xl">
+            <Accent>Projects</Accent>
+         </h1>
+         <p className="mt-2 text-gray-600 dark:text-gray-300">
+            My favorite projects.
+         </p>
+      </section>
    )
 }
 export default Projects
