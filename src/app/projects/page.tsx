473cf12ad37928933ee01f2d70a78bc80c29953a
LaupWing <loc_strict@hotmail.com>
2023-05-28 09:54:10 +0200

    container

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -9,6 +9,7 @@ const Projects = () => {
          <p className="mt-2 text-gray-600 dark:text-gray-300">
             My favorite projects.
          </p>
+         <ul className="mt-6 grid gap-4 sm:grid-cols-2 xl:grid-cols-3"></ul>
       </section>
    )
 }
