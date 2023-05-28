4e9b80603dd629f41a10666cc76d97e9deee2b0a
LaupWing <loc_strict@hotmail.com>
2023-05-28 18:27:18 +0200

    update

--- src/app/projects/[slug]/page.tsx
+++ src/app/projects/[slug]/page.tsx
@@ -1,4 +1,5 @@
 import { CloudinaryImage } from "@/components/images"
+import { getFiles } from "@/lib/mdx"
 
 const SingleProjectPage = () => {
    return (
@@ -13,3 +14,13 @@ const SingleProjectPage = () => {
    )
 }
 export default SingleProjectPage
+
+export const dynamicParams = false
+
+export async function generateStaticParams() {
+   const posts = await getFiles("projects")
+   
+   return posts.map((p) => ({
+      slug: p.replace(/\.mdx/, "")
+   }))
+}
