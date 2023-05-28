d427221e392ccdee473e43f1cb5bd9cdfe1cb6e3
LaupWing <loc_strict@hotmail.com>
2023-05-28 18:57:24 +0200

    update

--- src/components/links/CustomLink.tsx
+++ src/components/links/CustomLink.tsx
@@ -10,7 +10,7 @@ export const CustomLink:FC<UnstyledLinkProps> = ({
    return (
       <UnstyledLink
          {...props}
-         className={clsx("animated-underline inline-flex items-center font-medium focus:outline-none focus-visible:ring focus-visible:ring-accent-dark dark:focus-visible:ring-accent-light border-b border-dotted border-dark hover:border-dark/0", className)}
+         className={clsx("animated-underline inline-flex items-center font-medium focus:outline-none focus-visible:ring focus-visible:ring-accent-dark dark:focus-visible:ring-accent-light border-b border-dotted border-dark dark:border-light hover:border-dark/0", className)}
       >
          <span className="dark:bg-gradient-to-tr dark:from-accent-light dark:to-accent-dark dark:bg-clip-text dark:text-transparent">
             {children}
