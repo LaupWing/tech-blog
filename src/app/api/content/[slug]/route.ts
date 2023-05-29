117af4ea3f7d8d4f3aa4e634ab3ffd446d01a418
LaupWing <loc_strict@hotmail.com>
2023-05-29 17:41:40 +0200

    udpate

--- src/app/api/content/[slug]/route.ts
+++ src/app/api/content/[slug]/route.ts
@@ -9,21 +9,43 @@ export async function GET(req: Request) {
    })
 }
 
-export async function POST(req: Request) {
+export async function POST(req: NextRequest) {
    const sessionId = getSessionId(req)
-   // req.
-   // try {
-   //    // const content = await prismaClient.contentMeta.upsert({
-   //    //    where: {
-   //    //       slug: slug
-   //    //    },
-   //    //    create: {
-   //    //       slug: slug
-   //    //    }
-   //    // })
-   // } catch(e){
-   //    console.log(e)
-   // }
+   const splitted = req.url.split("/")
+   const slug = splitted[splitted.length - 1]
+   
+   try {
+      const content = await prismaClient.contentMeta.upsert({
+         where: {
+            slug: slug
+         },
+         create: {
+            slug: slug,
+            views: {
+               create: {
+                  sessionId: sessionId
+               }
+            }
+         },
+         update: {
+            views:{
+               create: {
+                  sessionId
+               }
+            }
+         },
+         include: {
+            _count: {
+               select: {
+                  views: true,
+                  likes: true
+               }
+            }
+         }
+      })
+   } catch(e){
+      console.log(e)
+   }
    
    return NextResponse.json({})
 }
