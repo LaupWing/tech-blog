ce40e0926973969eb3413cc51b159b2206fdc3c5
LaupWing <loc_strict@hotmail.com>
2023-05-29 09:27:09 +0200

    udpate

--- src/app/api/content/[slug]/route.ts
+++ src/app/api/content/[slug]/route.ts
@@ -1,9 +1,11 @@
+import { getSessionId } from "@/lib/helper.server"
+import { NextApiRequest, NextApiResponse } from "next"
 
 
 export async function GET() {
       
 }
 
-export async function POST() {
-
+export async function POST(req: NextApiRequest, res: NextApiResponse) {
+   const sessionId = getSessionId(req)
 }
