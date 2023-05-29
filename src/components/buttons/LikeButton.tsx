62969b6353c5e91cefc21a43c8dfcb98aad1a65b
LaupWing <loc_strict@hotmail.com>
2023-05-29 06:25:35 +0200

    update

--- src/components/buttons/LikeButton.tsx
+++ src/components/buttons/LikeButton.tsx
@@ -1,5 +1,17 @@
+import { FC } from "react"
+
 export const LikeButton = () => {
    return (
       <div>LikeButton</div>
+   )
+}
+
+const LikeButtonHeart:FC<{ likes: number }> = ({ likes = 6}) => {
+   return (
+      <div className="relative">
+         <div className="absolute w-full text-center text-2xl">
+
+         </div>
+      </div>
    )
 }
