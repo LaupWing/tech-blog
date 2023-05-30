af7afc0650ebb98aa0a6581f75ca4847999dda00
LaupWing <loc_strict@hotmail.com>
2023-05-30 19:07:50 +0200

    update

---
+++ src/app/api/like/[slug]/route.ts
@@ -0,0 +1,59 @@
+import { extractSlug, getSessionId } from "@/lib/helper.server"
+import { prismaClient } from "@/lib/prisma"
+import { NextResponse } from "next/server"
+
+
+export async function POST(req: Request) {
+   try {
+      const sessionId = getSessionId(req)
+      const slug = extractSlug(req)
+      const content = await prismaClient.contentMeta.upsert({
+         where: {
+            slug: slug
+         },
+         create: {
+            slug: slug,
+            Like: {
+               create: {
+                  sessionId: sessionId
+               }
+            }
+         },
+         update: {
+            Like:{
+               create: {
+                  sessionId: sessionId
+               }
+            }
+         },
+         include: {
+            _count: {
+               select: {
+                  Like: true,
+                  View: true
+               }
+            }
+         }
+      })
+      return NextResponse.json({
+         contentViews: content?._count.View ?? 0,
+         contentLikes: content?._count.Like ?? 0
+      }, {
+         status: 201
+      })
+   } catch(e: unknown){
+      if (e instanceof Error) {
+         return NextResponse.json({
+            message: e.message ?? "Internal Server Error"
+         }, {
+            status: 500
+         })
+      } else {
+         return NextResponse.json({
+            message: "Internal Server Error"
+         }, {
+            status: 500
+         })
+      }
+   }
+}
