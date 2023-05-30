2536e46a52f5034d1792dba48be786129cd54cdc
LaupWing <loc_strict@hotmail.com>
2023-05-30 06:07:40 +0200

    udpate

--- src/app/api/content/route.ts
+++ src/app/api/content/route.ts
@@ -1,6 +1,20 @@
+import { prismaClient } from "@/lib/prisma"
 import { NextResponse } from "next/server"
 
 export async function GET(req: Request) {
+   const content = await prismaClient.contentMeta.findMany({
+      include: {
+         _count: {
+            select: {
+               View: true,
+               Like: true
+            }
+         }
+      }
+   })
+   // Sort by slug
+   content.sort((a, b) => a.slug.localeCompare(b.slug))
+
    return NextResponse.json({
       "test": "test"
    })
