b449e9078feb8837e377fe04ee16e5ed24f813ad
LaupWing <loc_strict@hotmail.com>
2023-05-30 05:46:18 +0200

    header props

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -36,50 +36,7 @@ const SingleBlogPage = async (props: PageProps) => {
    return (
       <main>
          <section className="layout">
-            <div className="pb-4">
-               <CloudinaryImage
-                  publicId="samples/bike"
-                  alt="Bike"
-                  width={1200}
-                  height={(1200 * 2) /5}
-                  aspect={{
-                     height: 2,
-                     width: 5
-                  }}
-               />
-               <h1 className="mt-4">{frontmatter.title}</h1>
-               <p className="mt-2 text-sm text-gray-600 dark:text-gray-300">
-                  Writton on{" "}
-                  {format(new Date(frontmatter.publishedAt), "MMMM dd, yyyy")} by
-                  Laup Wing
-               </p>
-               {frontmatter.lastUpdated && (
-                  <div className="mt-2 flex flex-wrap gap-2 text-sm text-gray-700 dark:text-gray-200">
-                     <p>
-                        Last updated{" "}
-                        {format(new Date(frontmatter.lastUpdated), "MMMM dd, yyyy")}
-                        .
-                     </p>
-                     <UnstyledLink
-                        href={COMMIT_HISTORY_LINK}
-                        className="inline-flex items-center gap-1 rounded-sm font-medium text-gray-600 hover:text-gray-900 dark:text-gray-300 dark:hover:text-accent-light focus:outline-none focus-visible:ring focus-visible:ring-accent-dark"
-                     >
-                        <IconHistory className="text-lg" />
-                        <span>See changes</span>
-                     </UnstyledLink>
-                  </div>
-               )}
-               <div className="mt-6 flex items-center justify-start gap-2 text-sm font-medium text-gray-600 dark:text-gray-300">
-                  <div className="flex items-center gap-1">
-                     <IconClock className="inline-block text-base" />
-                     <Accent>{frontmatter.readingTime.text}</Accent>
-                  </div>
-                  <div className="flex items-center gap-1">
-                     <IconEye className="text-base inline-block" />
-                     <Accent>--- views</Accent>
-                  </div>
-               </div>
-            </div>
+            
 
             <hr className="dark:border-gray-600" />
             <section className="lg:grid lg:grid-cols-[auto,250px] lg:gap-8">
