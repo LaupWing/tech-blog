6d1c4e239126618973067d54b6c2bb210e15a463
LaupWing <loc_strict@hotmail.com>
2023-05-30 19:18:19 +0200

    check

--- src/lib/helper.server.ts
+++ src/lib/helper.server.ts
@@ -13,7 +13,7 @@ export const getSessionId = (req: Request) => {
 
 export const extractSlug = (req: Request) => {
    const splitted = req.url.split("/")
-   
+   // Need to add content and like check
 
    if(splitted[splitted.length - 2] === "content"){
       const slug = z.string().parse(splitted[splitted.length - 1])
