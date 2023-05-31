e3e1ed8bb13cc12ea3cba014b2139586cc198934
LaupWing <loc_strict@hotmail.com>
2023-05-31 13:52:53 +0200

    update

--- src/app/library/PostsSection.tsx
+++ src/app/library/PostsSection.tsx
@@ -1,6 +1,8 @@
 "use client"
-import { LibraryFrontmatter } from "@/types/frontmatters"
-import { ChangeEvent, FC, useState } from "react"
+import { Tag } from "@/components/elements"
+import { getTags } from "@/lib/mdx-client"
+import { InjectedMeta, LibraryFrontmatter } from "@/types/frontmatters"
+import { ChangeEvent, FC, useEffect, useState } from "react"
 
 interface PostsSectionProps {
    posts: Array<LibraryFrontmatter>
@@ -10,9 +12,38 @@ const PostsSection:FC<PostsSectionProps> = ({
    posts
 }) => {
    const [search, setSearch] = useState<string>("")
+   const [filteredPosts, setFilteredPosts] = useState<Array<LibraryFrontmatter & InjectedMeta>>(
+      () => [...posts]
+   )
+
+   useEffect(() => {
+      const result = posts.filter((post) =>
+         post.title.toLowerCase().includes(search.toLowerCase()) ||
+         post.description.toLowerCase().includes(search.toLowerCase()) ||
+         search.toLowerCase()
+            .split(" ")
+            .every((tag) => post.tags.includes(tag))
+      )
+      setFilteredPosts(result)
+   }, [search])
+
    const handleSearch = (e: ChangeEvent<HTMLInputElement>) => {
       setSearch(e.target.value)
    }
+   const tags = getTags(posts)
+   const toggleTag = (tag: string) => {
+      if(search.includes(tag)){
+         setSearch((s) =>
+            s
+               .split(" ")
+               .filter((t) => t !== tag)
+               .join(" ")
+         )
+      } else {
+         setSearch((s) => (s !== "" ? `${s.trim()} ${tag}` : tag))
+      }
+   }
+   const filteredTags = getTags(filteredPosts)
 
    return (
       <>
@@ -23,6 +54,18 @@ const PostsSection:FC<PostsSectionProps> = ({
             value={search}
             onChange={handleSearch}
          />
+         <div className="mt-2 flex flex-wrap items-baseline justify-start gap-2 text-sm text-gray-600 dark:text-gray-300">
+            <span className="font-medium">Choose topic:</span>
+            {tags.map((tag) => (
+               <Tag
+                  key={tag}
+                  onClick={() => toggleTag(tag)}
+                  disabled={!filteredTags.includes(tag)}
+               >
+                  {tag}
+               </Tag>
+            ))}
+         </div>
       </>
    )
 }
