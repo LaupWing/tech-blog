2c5ddf29b9f569c9910f6530688a2000c92f8261
LaupWing <loc_strict@hotmail.com>
2023-05-31 09:41:37 +0200

    fetching library

--- src/app/library/page.tsx
+++ src/app/library/page.tsx
@@ -1,4 +1,14 @@
-const Library = () => {
+import { getAllFilesFrontmatter } from "@/lib/mdx"
+
+const fetchLibrary = async () => {
+   const library = await getAllFilesFrontmatter("library")
+
+   return library
+}
+
+const Library = async () => {
+   const library = await fetchLibrary()
+
    return (
       <div>Library</div>
    )
