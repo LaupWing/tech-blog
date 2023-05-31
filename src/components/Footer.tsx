134d1af2e5f429b0592e539ab66d84f585ae26ca
LaupWing <loc_strict@hotmail.com>
2023-05-31 12:11:26 +0200

    update

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,5 @@
 import { FC, ReactNode } from "react"
-import { IconType } from "react-icons"
+import { IconType } from "@react-icons/all-files"
 import { IconGithub, IconLinkedin, IconTwitter } from "./Icons"
 import { Accent } from "./elements/Accent"
 import { Tooltip } from "./Tooltip"
--- src/components/elements/SortListBox.tsx
+++ src/components/elements/SortListBox.tsx
@@ -1,5 +1,5 @@
 "use client"
-import { IconType } from "react-icons"
+import { IconType } from "@react-icons/all-files"
 import { Dispatch, FC, SetStateAction, Fragment } from "react"
 import { Listbox, Transition } from "@headlessui/react"
 import { IconCheckmark, IconSelector } from "../Icons"
