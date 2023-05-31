a367184c4f67870cdb86ce5fd1aa278507d27513
LaupWing <loc_strict@hotmail.com>
2023-05-31 13:21:26 +0200

    update

--- src/app/library/PostsSection.tsx
+++ src/app/library/PostsSection.tsx
@@ -1,8 +1,24 @@
 "use client"
+import { LibraryFrontmatter } from "@/types/frontmatters"
+import { FC } from "react"
 
-const PostsSection = () => {
+interface PostsSectionProps {
+   posts: Array<LibraryFrontmatter>
+}
+
+const PostsSection:FC<PostsSectionProps> = ({
+   posts
+}) => {
    return (
-      <div>PostsSection</div>
+      <>
+         {/* <input 
+            type="text" 
+            className="w-full rounded-md dark:bg-dark border border-gray-300 dark:border-gray-600 focus:border-accent-dark focus:outline-none focus:ring-0 dark:focus:border-accent-light mt-4"
+            placeholder="Search..."
+            value={search}
+            onChange={handleSearch}
+         /> */}
+      </>
    )
 }
 export default PostsSection
