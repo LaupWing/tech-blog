91cdcd8a067c04e61dfa4923c89cee6ca340e844
LaupWing <loc_strict@hotmail.com>
2023-05-31 13:22:40 +0200

    search

--- src/app/library/PostsSection.tsx
+++ src/app/library/PostsSection.tsx
@@ -1,6 +1,6 @@
 "use client"
 import { LibraryFrontmatter } from "@/types/frontmatters"
-import { FC } from "react"
+import { ChangeEvent, FC, useState } from "react"
 
 interface PostsSectionProps {
    posts: Array<LibraryFrontmatter>
@@ -9,15 +9,20 @@ interface PostsSectionProps {
 const PostsSection:FC<PostsSectionProps> = ({
    posts
 }) => {
+   const [search, setSearch] = useState<string>("")
+   const handleSearch = (e: ChangeEvent<HTMLInputElement>) => {
+      setSearch(e.target.value)
+   }
+
    return (
       <>
-         {/* <input 
+         <input 
             type="text" 
             className="w-full rounded-md dark:bg-dark border border-gray-300 dark:border-gray-600 focus:border-accent-dark focus:outline-none focus:ring-0 dark:focus:border-accent-light mt-4"
             placeholder="Search..."
             value={search}
             onChange={handleSearch}
-         /> */}
+         />
       </>
    )
 }
