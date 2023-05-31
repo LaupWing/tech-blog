be1462c9a52572e350798f69b71df6eddb9febb6
LaupWing <loc_strict@hotmail.com>
2023-05-31 11:41:41 +0200

    update

--- src/hooks/useInjectContentMeta.tsx
+++ src/hooks/useInjectContentMeta.tsx
@@ -1,7 +1,8 @@
 "use client"
-import { ContentType, PickFrontmatter } from "@/types/frontmatters"
+import { pickContentMeta } from "@/lib/helpers"
+import { ContentType, InjectedMeta, PickFrontmatter } from "@/types/frontmatters"
 import { ContentMeta } from "@/types/meta"
-import { useMemo } from "react"
+import { useMemo, useState, useEffect } from "react"
 import useSwr from "swr"
 
 export default function useInjectContentMeta<T extends ContentType>(
@@ -11,5 +12,32 @@ export default function useInjectContentMeta<T extends ContentType>(
    const { data, isLoading } = useSwr<Array<ContentMeta>>(
       "/api/content"
    )
-   const meta = useMemo
+   const meta = useMemo(
+      () => pickContentMeta(data, type),
+      [data, type]
+   )
+
+   type PopulatedContent = Array<PickFrontmatter<T> & InjectedMeta>
+
+   const [populatedContent, setPopulatedContent] = useState<PopulatedContent>(
+      () => [...frontmatter] as PopulatedContent
+   )
+
+   useEffect(() => {
+      if (meta){
+         const mapped = frontmatter.map((fm) => {
+            const views = meta.find(meta => meta.slug === fm.slug)?.views
+            const likes = meta.find(meta => meta.slug === fm.slug)?.likes
+
+            return {
+               ...fm,
+               views: views,
+               likes: likes
+            }
+         })
+         setPopulatedContent(mapped)
+      }
+   }, [])
+
+   return populatedContent
 }
