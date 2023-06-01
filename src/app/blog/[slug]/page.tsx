f4bf9415cfa066407fc4738aa33d74845966a1b9
LaupWing <loc_strict@hotmail.com>
2023-06-01 10:22:49 +0200

    update

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -2,6 +2,8 @@
 import { getFileBySlug, getFiles } from "@/lib/mdx"
 import { BlogFrontmatter } from "@/types/frontmatters"
 import ContentSection from "./ContentSection"
+import { Metadata } from "next"
+import { defaultMeta } from "@/config"
 
 export const dynamicParams = false
 
@@ -11,6 +13,15 @@ export async function generateStaticParams() {
    return posts.map((p) => ({
       slug: p.replace(/\.mdx/, "")
    }))
+}
+
+export async function generateMetadata(props: PageProps): Promise<Metadata> {
+   const post = await fetchPost(props.params.slug)
+   const { frontmatter } = post
+   return {
+      title: `${frontmatter.title} | ${defaultMeta.siteName}`,
+      description: frontmatter.description,
+   }
 }
 
 const fetchPost = async (slug: string) => {
