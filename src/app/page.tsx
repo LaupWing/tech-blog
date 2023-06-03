b70c4f341bb4c2a9c51f2477b32894118bef0d38
LaupWing <loc_strict@hotmail.com>
2023-06-03 09:35:39 +0200

    udpate

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,3 @@
-import { getAllFilesFrontmatter, getRecent } from "@/lib/mdx"
 import { generateRss } from "@/lib/rss"
 import HomeIntro from "./HomeIntro"
 import HomeProjects from "./HomeProjects"
@@ -6,29 +5,8 @@ import HomeBlogs from "./HomeBlogs"
 import { Suspense } from "react"
 import HomeLibrary from "./HomeLibrary"
 
-const fetchFrontmatters = async () => {
-   generateRss()
-   const blogs = await getAllFilesFrontmatter("blog")
-   const projects = await getAllFilesFrontmatter("projects")
-   const library = await getAllFilesFrontmatter("library")
-
-   const recentBlogs = getRecent(blogs)
-   const recentProjects = getRecent(projects)
-   const recentLibrary = getRecent(library)
-
-   return {
-      recentBlogs,
-      recentLibrary,
-      recentProjects
-   }
-}
-
 export default async function Home() {
-   const {
-      recentBlogs, 
-      recentLibrary, 
-      recentProjects} = await fetchFrontmatters()
-   
+   generateRss()
    return (
       <main>
          <HomeIntro />
