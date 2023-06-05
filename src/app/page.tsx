e07e5978d0cf8a8ac6ec42d7892098a084a5ef73
LaupWing <loc_strict@hotmail.com>
2023-06-05 05:57:05 +0200

    redoing stuff

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 import { generateRss } from "@/lib/rss"
 import HomeIntro from "@/app/components/Home/HomeIntro"
 import HomeProjects from "@/app/components/Home/HomeProjects"
-import HomeBlogs from "@/app/components/Home/HomeBlogs"
 import { Suspense } from "react"
 import HomeLibrary from "@/app/components/Home/HomeLibrary"
 
@@ -27,3 +26,32 @@ export default async function Home() {
 }
 
 
+
+const HomeBlogs = async () => {
+   const recentBlogs = await fetchRecentBlogs()
+
+   return (
+      <section className="py-20">
+         <article className="layout">
+            <h2 id="projects" className="text-2xl md:text-4xl">
+               <Accent>Recent Blog Posts</Accent>
+            </h2>
+            <ul className="mt-4 grid gap-4 sm:grid-cols-2 xl:grid-cols-3">
+               {recentBlogs.map((post, i) => (
+                  <BlogCard 
+                     key={post.slug}
+                     post={post}
+                  />
+               ))}
+            </ul>
+            <ButtonLink
+               className="mt-4"
+               href="/blog"
+               // Add tracking event
+            >
+               See more post
+            </ButtonLink>
+         </article>
+      </section>
+   )
+}
