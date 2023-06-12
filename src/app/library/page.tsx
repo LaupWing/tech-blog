257948ce65d9c1bce7939138aa7aad58fc45f3b5
LaupWing <loc_strict@hotmail.com>
2023-06-12 14:43:44 +0200

    LIbrary seo

--- src/app/library/page.tsx
+++ src/app/library/page.tsx
@@ -1,11 +1,21 @@
 import { Accent } from "@/components/elements/Accent"
 import { getAllFilesFrontmatter } from "@/lib/mdx"
 import PostsSection from "./PostsSection"
+import { Metadata } from "next"
+import seo from "@/lib/seo"
 
 const fetchLibrary = async () => {
    const library = await getAllFilesFrontmatter("library")
 
    return library
+}
+
+export const metadata: Metadata = {
+   ...seo({
+      asPath: "library",
+      title: "Library Page",
+      description: "Here's a collection of code snippets from various technologies that I have used before. Feel free to make use of them!"
+   })
 }
 
 const Library = async () => {
